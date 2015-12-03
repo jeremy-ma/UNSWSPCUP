88e99015d22ef845af2cec0c75f93d03d0edec1b
rossleon <rossleon.aus@gmail.com>
2015-12-03 18:37:39 +1100

    Wavelet Transform re-written

--- example_classifier/find_wt_var.m
+++ example_classifier/find_wt_var.m
@@ -10,8 +10,9 @@ function feature_v = find_wt_var(enf, feature_v, lvl, wname)
     c_var = zeros(size(enf,2),lvl+1);
     for i=1:size(enf,2)
         [c,l] = wavedec(enf(:,i),lvl,wname);
-        start_ind = [0; l(2:end-1)] + 1;
-        end_ind = l(2:end);
+        l_sum = cumsum(l(1:end-1));
+        start_ind = [0; l_sum(1:end-1)] + 1;
+        end_ind = l_sum;
         for j=1:lvl+1
             c_var(i,j) = log(var(c(start_ind(j):end_ind(j))));
         end
--- example_classifier/main.m
+++ example_classifier/main.m
@@ -10,7 +10,6 @@ grid_num = 'all';
 record_type = 'P';
 record_num = 'all';
 
-% kfold = 5;
 
 frame_size = 5;    %size of a frame (ENF point) in seconds, default 5
 overlap_enf = 0;   %overlap between frames, default 0
@@ -20,40 +19,31 @@ overlap_seg = 0;   %overlap between segments, default 0
 
 features_num = 10;  
 features_weight = ones(1,features_num);        %assign weight to different features (size = 1 x feature_size, eg 1x10)
+
+train_w = 0.7;
+valid_w = 0.3;
 %%
 change_dir(orig_dir, path_train, store_fig);
 
-%train data
+%create file list
 [file_list_train, file_tab_train, file_label_train] = list_files(path_train,grid_num,record_type,record_num);
-% [features_train,label_concat] = features_extract(path_train, file_list_train, label_train, frame_size, overlap_enf, nfft, seg_size, overlap_seg);
+[file_list_test,num_test] = list_files_test(path_test);
+
+%ENF extraction + feature extraction
 train_data = FeaturesExtractClass(frame_size, overlap_enf, nfft, seg_size, overlap_seg);
 train_data = train_data.features_extract(path_train, file_list_train);
 train_data = train_data.labelling(file_label_train);
-
-% [norm_train,grad,intercept] = feature_norm(features_train);
-scale_coeff = FeaturesNormClass(train_data.features,features_weight);
-train_data.features_norm = scale_coeff.scaletest(train_data.features);
-
-%test data
-[file_list_test,num_test] = list_files_test(path_test);
 test_data = FeaturesExtractClass(frame_size, overlap_enf, nfft, seg_size, overlap_seg);
 test_data = test_data.features_extract(path_test, file_list_test);
 test_data = test_data.labelling(num2str(num_test,'%02i'));
 
+%Features normalisation
+scale_coeff = FeaturesNormClass(train_data.features,features_weight);
+train_data.features_norm = scale_coeff.scaletest(train_data.features);
 test_data.features_norm = scale_coeff.scaletest(test_data.features);
 
-%SVM
-train_w = 0.7;
-valid_w = 0.3;
-% test_w = 0;
+%Classification
+
 class_algo = {@SVMClass};
 store_text = {'SVM'};
 classification(class_algo,store_text,train_data,train_w,valid_w,test_data,store_fig);
-% SVMobj = SVMClass();
-% SVMobj = SVMobj.split_data(train_data,train_w,valid_w);
-% SVMobj = SVMobj.training();
-% [pred_label,score] = SVMobj.testing(SVMobj.valid_data);
-% [SVMobj, merge_label, conf_prob_merge] = SVMobj.merge_seg(pred_label, score, SVMobj.valid_tag);
-% SVMobj.confuse(merge_label,store_fig, conf_prob_merge);
-
-% [prob_m, accuracy] = svm_train(train_norm,label_concat,kfold,store_fig);
