0703dac42b790e09c8e9014703db6bfe1a1d1b30
androoX <z3418586@student.unsw.edu.au>
2015-12-14 19:38:38 +1100

    update (not sure if runs)

--- fmatrixpgram.m
+++ fmatrixpgram.m
@@ -18,13 +18,26 @@ timeshift = framelength - overlap;
 noframe = floor((length(y)-framelength)/timeshift);
 % NOTE I AM CURRENTLY USING A RECTANGULAR MATRIX. IS THAT GOOD? BAD?
 window = zeros(0,0);
-fmatrix = zeros(peakno, noframe+1);
+fmatrix = zeros(2*peakno, noframe+1);
 
 % Take the periodogram of each frame & the indexes of each frame
-for i = 0:noframe
+pxx = periodogram(y(1:framelength), window, nfft);
+fiflowind = floor((2*48*(length(pxx)-1))/fs);
+fifhighind = ceil((2*52*(length(pxx)-1))/fs);
+sixlowind = floor((2*58*(length(pxx)-1))/fs);
+sixhighind = ceil((2*62*(length(pxx)-1))/fs);
+[peaksfif, peakindexfif] = sort(pxx(fiflowind:fifhighind), 'descend');
+[peakssix, peakindexsix] = sort(pxx(sixlowind:sixhighind), 'descend');
+fmatrix(1:peakno,1) = peakindexfif(1:peakno)+fiflowind-1;
+fmatrix(peakno+1:2*peakno, 1) = peakindexsix(1:peakno) + sixlowind-1;
+
+for i = 1:noframe
     pxx = periodogram(y(i*timeshift+1:i*timeshift+framelength), window, nfft);
-    [peaks, peakindex] = sort(pxx, 'descend');
-    fmatrix(:,i+1) = peakindex(1:peakno);
+    
+    [peaksfif, peakindexfif] = sort(pxx(fiflowind:fifhighind), 'descend');
+    [peakssix, peakindexsix] = sort(pxx(sixlowind:sixhighind), 'descend');
+    fmatrix(1:peakno/2,i+1) = peakindexfif(1:peakno/2)+fiflowind-1;
+    fmatrix((peakno/2)+1:peakno,i+1) = peakindexsix(1:peakno/2) + sixlowind-1;
 end
 
 %translate the inddexes into the frequencies 
