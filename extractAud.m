910e95be485116da19e55d6d3f34415fa59cb990
androoX <z3418586@student.unsw.edu.au>
2015-12-30 18:32:55 +1100

    fix

--- extractAud.m
+++ extractAud.m
@@ -2,7 +2,7 @@ function [enf, time] = extractAud(y,fs)
 
 [ynew, fsnew] = bandpass_process_nharmonics(y, fs, 60, 0.1, 4, 0.5);
 freqmat = quad_interpolate_multipeak(ynew, fsnew, 10000, 9000, 40000, 15);
-path = leastcostENF(fmatrix);
+path = leastcostENF(freqmat');
 time = [1:length(path)];
 enf = path;
 
