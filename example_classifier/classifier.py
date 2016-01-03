926f3e79d78b4a00d5c229f3cdacd74a7b7356e6
jeremy-ma <jeremy_ma@outlook.com>
2016-01-03 02:55:01 -0800

    minor update to classifier

--- example_classifier/classifier.py
+++ example_classifier/classifier.py
@@ -3,9 +3,11 @@ import numpy as np
 import pdb
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+from sklearn.ensemble import AdaBoostClassifier
 
-def loadData():
-	data = sio.loadmat('trainTestData.mat')
+def loadData(filename='trainTestData.mat'):
+	data = sio.loadmat(filename)
 	trainFeatures = data['trainFeatureVectors']
 	testFeatures = data['testFeatureVectors']
 	trainLabels = data['trainLabels']
@@ -17,10 +19,18 @@ def loadData():
 
 if __name__ == '__main__':
 	trainFeatures,trainLabels,testFeatures,testLabels = loadData()
-	classifier = svm.SVC(kernel='linear')
+	#trainFeatures2, trainLabels2, testFeatures2, testLabels2 = loadData('AndrewFeatures/trainTestData.mat')
+	#pdb.set_trace()
+
+	"""
+	min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-100,100))
+	trainFeatures = min_max_scaler.fit_transform(trainFeatures)
+	testFeatures = min_max_scaler.transform(testFeatures)
+	"""
+	classifier = svm.SVC(kernel='rbf', C=1)
 	classifier.fit(trainFeatures, trainLabels)
 	predicted = classifier.predict(testFeatures)
-	print predicted
+	print len(predicted)
 	print testLabels
 	#pdb.set_trace()
 	print accuracy_score(testLabels,predicted)
