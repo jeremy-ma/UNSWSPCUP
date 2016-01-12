9364e5f97b0166d49fd83322580f0ed3e18d6ec0
jeremy-ma <jeremy_ma@outlook.com>
2016-01-12 00:14:43 -0800

    use ExtraTreesClassifier instead, increases accuracy to 0.92

--- example_classifier/classifier.py
+++ example_classifier/classifier.py
@@ -7,9 +7,11 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn import preprocessing
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier, ExtraTreesClassifier, GradientBoostingClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import GaussianNB
+import matplotlib.pyplot as plt
+
 
 def evaluate(classifier,trainFeatures,trainLabels,testFeatures,testLabels):
     results = {}
@@ -24,10 +26,11 @@ def evaluate(classifier,trainFeatures,trainLabels,testFeatures,testLabels):
         outlier = onevsAll[prediction].predict(features)[0]
         #outlier = oneClassSVM.predict(features)
         if outlier == True:
+            #pass
             predictions[i] = helper.UNKNOWN
-            print "#############################################"
-            print onevsAll[prediction].predict_proba(features)[0]
-            print testLabels[i]
+            #print "#############################################"
+            #print onevsAll[prediction].predict_proba(features)[0]
+            #print testLabels[i]
     
     results['predicted'] = predictions
     results['testAccuracy'] = accuracy_score(testLabels, results['predicted'])
@@ -48,6 +51,7 @@ def trainOneVsAll(trainFeatures, trainLabels):
         clf = svm.SVC(kernel='rbf', probability=True, class_weight={False:0.9, True:0.1})
         #clf = svm.SVC(kernel='poly',degree=3,probability=True, class_weight={False:0.9, True:0.1})
         #clf = RandomForestClassifier(n_estimators=1000,class_weight={False:0.9, True:0.01})
+        #clf = ExtraTreesClassifier(n_estimators=2000,class_weight={False:0.95, True:0.01})
         classifiers[label] = clf
 
     for label, classifier in classifiers.iteritems():
@@ -56,10 +60,35 @@ def trainOneVsAll(trainFeatures, trainLabels):
 
     return classifiers
 
+def featureImportance(X,y):
+    forest = ExtraTreesClassifier(n_estimators=2000,
+                              random_state=0)
+
+    forest.fit(X, y)
+    importances = forest.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+                 axis=0)
+    indices = np.argsort(importances)[::-1]
+
+    # Print the feature ranking
+    print("Feature ranking:")
+
+    for f in range(X.shape[1]):
+        print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+    # Plot the feature importances of the forest
+    plt.figure()
+    plt.title("Feature importances")
+    plt.bar(range(X.shape[1]), importances[indices],
+           color="r", yerr=std[indices], align="center")
+    plt.xticks(range(X.shape[1]), indices)
+    plt.xlim([-1, X.shape[1]])
+    plt.show()
+
 
 if __name__ == '__main__':
     #trainFeatures,trainLabels,testFeatures,testLabels = helper.randomLoadData('trainTestData.mat')
-    trainFeatures, trainLabels, testFeatures, testLabels,_,_ = helper.loadData('trainTestData.mat')
+    trainFeatures, trainLabels, testFeatures, testLabels,_,_ = helper.loadData('trainTestDataAndrew.mat')
 
     #pdb.set_trace()
     #min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-100,100))
@@ -72,12 +101,12 @@ if __name__ == '__main__':
 
     print trainFeatures.shape
 
-    classifier = RandomForestClassifier(n_estimators=2000, oob_score = True, min_samples_split=1)
-
-
+    #classifier = RandomForestClassifier(n_estimators=2000, oob_score = True, min_samples_split=1)
+    #classifier = GradientBoostingClassifier(n_estimators=1000)
+    classifier = ExtraTreesClassifier(n_estimators=2000, oob_score=True, bootstrap=True)
     results = evaluate(classifier, trainFeatures,trainLabels,testFeatures,testLabels)
     print results
-
+    print results['testAccuracy'], results['trainAccuracy']
     """
     testUnknownLabels = [label==(ord('N')-ord('A')) for label in testLabels]
 
@@ -92,3 +121,4 @@ if __name__ == '__main__':
     rows = resultsUnknown['predicted'] == unknownTestLabels
     print resultsUnknown['probability'][rows]
     """
+    featureImportance(trainFeatures,trainLabels)
