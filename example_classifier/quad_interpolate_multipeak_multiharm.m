b0a4a20add48f6554cedb5d6363d4ca373ec99d2
Adrian Muljadi <a.muljadi@unsw.edu.au>
2016-01-14 20:15:11 +1100

    added andrew's stuff

--- example_classifier/quad_interpolate_multipeak_multiharm.m
+++ example_classifier/quad_interpolate_multipeak_multiharm.m
@@ -1,4 +1,4 @@
-function freqmat = quad_interpolate_multipeak(y, fs, framelength, overlap, nfft, nopeaks)
+function freqmat = quad_interpolate_multipeak_multiharm(y, fs, framelength, overlap, nfft, nopeaks)
 
 % NFFT points is 4*length of framelen described in the paper.
 
@@ -6,7 +6,8 @@ frame_hop = framelength - overlap;
 number_frames = floor((length(y)-framelength)/frame_hop);
 freqmat = zeros(nopeaks, number_frames+1);
 %Get the frame of concern
-frames_temp_M = buffer(y,framelength,overlap,'nodelay');
+frames_temp_M = buffer(y,framelength,overlap, 'nodelay');
+
 %Take the fft of the frame of concern
 fft_signal_M = abs(fft(frames_temp_M, nfft));
 fft_signal_M = fft_signal_M(1:(size(fft_signal_M,1))/2,:);
@@ -23,7 +24,17 @@ for i = 0:number_frames
     frame_freq_index_M = potential_peaks+quadratic_maxima_M;
     freqmat(:,i+1) =  (frame_freq_index_M/nfft)*fs;
 end
-
-
+for n = 1:nopeaks
+    for m = 1:number_frames+1
+        if freqmat(n,m) < 80
+            continue
+        elseif freqmat(n,m) < 135
+            freqmat(n,m) = freqmat(n,m)/2;
+        elseif freqmat(n,m) < 190
+            freqmat(n,m) = freqmat(n,m)/3;
+        else 
+            freqmat(n,m) = freqmat(n,m)/4;
+        end
+    end
 end
-
+end
